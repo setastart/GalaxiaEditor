0f75254a114b92230cae6daad5b84333335dbe73
Ino <ino@detelic.pt>
2020-05-05 02:47:21 +0200

    bump galaxia-core dependency version

--- src/boot-web-editor.php
+++ src/boot-web-editor.php
@@ -59,7 +59,7 @@ Director::timerStart('editor');
 $editor = Director::initEditor(dirname(__DIR__));
 $geConf = [];
 require $app->dir . 'config/editor.php';
-$editor->version = '3.5.2';
+$editor->version = '3.5.3';
 Director::timerStop('editor');
 
 Director::loadTranslations();
