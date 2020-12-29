e8b07690f3ba5041f093dae4fc2c8a5a46f736e9
Ino <ino@detelic.pt>
2020-12-29 17:02:38 +0100

    GalaxiaEditor Version 4.27.7
    - fix form css

--- src/boot-web-editor.php
+++ src/boot-web-editor.php
@@ -48,7 +48,7 @@ Director::timerStart('editor');
 $editor = Director::initEditor(dirname(__DIR__));
 $geConf = [];
 require $app->dir . 'config/editor.php';
-$editor->version = '4.27.6';
+$editor->version = '4.27.7';
 Director::timerStop('editor');
 
 Director::loadTranslations();
