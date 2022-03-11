78cf70e9e438e793e6eaae41d7c9befb2f76bfa2
Ino <ino@detelic.pt>
2022-03-11 09:14:37 +0000

    GalaxiaEditor Version 5.8.3
    - Add hide-perm to list header links

--- src/boot-web-editor.php
+++ src/boot-web-editor.php
@@ -39,7 +39,7 @@ G::timerStart('editor');
 G::initEditor(dirname(__DIR__));
 E::$conf = require G::dir() . 'config/editor.php';
 
-G::$editor->version = '5.8.2';
+G::$editor->version = '5.8.3';
 
 G::timerStop('editor');
 
