cd5e6066eaf4d3b6d98c41d8b1270ebe0555dbab
Ino <ino@setastart.com>
2022-06-30 13:02:49 +0200

    GalaxiaEditor Version 5.47.3
    - Update README.md
    - Update .gitignore

--- src/boot-web-editor.php
+++ src/boot-web-editor.php
@@ -37,7 +37,7 @@ G::timerStop('Locales');
 G::timerStart('Editor');
 G::initEditor(dirname(__DIR__));
 
-G::$editor->version = '5.47.2';
+G::$editor->version = '5.47.3';
 
 G::timerStop('Editor');
 
