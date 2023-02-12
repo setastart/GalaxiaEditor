43183a603e16d2a7aa04e6df97ea5dd21110130e
Ino <ino@setastart.com>
2023-02-12 15:05:28 +0100

    GalaxiaEditor Version 5.80.1
    - Update copyright date
    - Update Text::TEST_HTML

--- src/Galaxia/core/RedisCli.php
+++ src/Galaxia/core/RedisCli.php
@@ -1,17 +1,32 @@
 <?php
-/*
- Copyright 2017-2022 Ino Detelić & Zaloa G. Ramos
-
-  - Licensed under the EUPL, Version 1.2 only (the "Licence");
-  - You may not use this work except in compliance with the Licence.
-
-  - You may obtain a copy of the Licence at: https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12
-
-  - Unless required by applicable law or agreed to in writing, software distributed
-    under the Licence is distributed on an "AS IS" basis,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  - See the Licence for the specific language governing permissions and limitations under the Licence.
+/**
+ * MIT License
+ *
+ * Copyright (c) 2016 Arminas Žukauskas - arminas@ini.lt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
+// Copyright 2017-2023 Ino Detelić & Zaloa G. Ramos (setastart.com)
+// Licensed under the European Union Public License, version 1.2 (EUPL-1.2)
+// You may not use this work except in compliance with the Licence.
+// Licence copy: https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12
 
 namespace Galaxia;
 
--- test/RedisCliTest.php
+++ test/RedisCliTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Copyright 2017-2022 Ino Detelić & Zaloa G. Ramos
+ Copyright 2017-2023 Ino Detelić & Zaloa G. Ramos
 
   - Licensed under the EUPL, Version 1.2 only (the "Licence");
   - You may not use this work except in compliance with the Licence.
