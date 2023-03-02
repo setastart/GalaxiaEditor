e5b1b6163bd50b9bc871e0e3db28c271e95cb396
Ino <ino@setastart.com>
2023-03-02 18:24:36 +0100

    GalaxiaEditor Version 5.82.1
    - Add EnvironmentTest.php script

---
+++ test/EnvironmentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Galaxia\RedisCli;
+
+include_once dirname(__DIR__) . '/src/boot-cli-editor.php';
+
+
+$redis = new RedisCli(host: 'localhost', port: '6379');
+
+
+function exitWithError($msg): never {
+    echo("️🛑 Error: " . $msg . PHP_EOL);
+    exit(1);
+}
+
+
+if (is_resource($redis->handle)) {
+    $redis->setErrorFunction(function($error) {
+        exitWithError('RedisCli: ' . __METHOD__ . ':' . __LINE__ . ' ' . $error);
+    });
+    if ($redis->cmd('PING')->get() != 'PONG') {
+        exitWithError('RedisCli: PING does not respont PONG');
+    }
+} else {
+    exitWithError('Redis: Connection Failed');
+}
+
+
+$in = dirname(__DIR__) . '/public/edit/favicon.pnqg';
+$vips = vips_image_new_from_file($in)['out'] ?? false;
+if (!$vips) {
+    exitWithError('Vips: Could not load vips image.');
+}
