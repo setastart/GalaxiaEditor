6182fb1e9df4acccac1810f61e7743168c4dc5be
Ino <ino@setastart.com>
2024-05-01 15:58:41 +0200

    GalaxiaEditor Version 6.7.2
    - Update Copyright year

--- src/Galaxia/core/RedisCli.php
+++ src/Galaxia/core/RedisCli.php
@@ -23,7 +23,7 @@
  * SOFTWARE.
  */
 
-// Copyright 2017-2023 Ino Detelić & Zaloa G. Ramos (setastart.com)
+// Copyright 2017-2024 Ino Detelić & Zaloa G. Ramos (setastart.com)
 // Licensed under the European Union Public License, version 1.2 (EUPL-1.2)
 // You may not use this work except in compliance with the Licence.
 // Licence copy: https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12
